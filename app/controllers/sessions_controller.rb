8a94ee889442bf72d10657f5204466c6909de44f
Helen <hu.heleny@gmail.com>
2017-10-10 16:25:12 -0400

    Add advanced login and remember me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		# Log the current user in and redirect to user's show page.
   		log_in user
-  		flash[:success] = "Welcome to the ToDo app!"
+  		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user
   	else
   		# Create an error message
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 end
